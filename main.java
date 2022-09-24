589916e47f8a281bfce5b0ce5ddae73ef43b854f
swatipal1010 <110754474+swatipal1010@users.noreply.github.com>
2022-09-24 12:27:28 +0530

    Update main.java

--- main.java
+++ main.java
@@ -17,3 +17,30 @@ public class project {
 
             System.out.println("Guess the number:");
             guess = sc.nextInt();
+            
+            
+        if (number == guess) {
+                System.out.println("Congratulations!" + " You guessed the number.");
+                break;
+            }
+            else if (number > guess
+                    && i != K - 1) {
+                System.out.println("The number is " + "greater than " + guess);
+            }
+            else if (number < guess
+                    && i != K - 1) {
+                System.out.println("The number is" + " less than " + guess);
+            }
+        }
+
+        if (i == K) {
+            System.out.println("You have exhausted" + " K trials.");
+
+            System.out.println("The number was " + number);
+        }
+    }
+    public static void
+    main(String arg[])
+    {
+        guessingNumberGame();
+    }
